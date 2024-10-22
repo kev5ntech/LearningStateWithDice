23fc60c309db736431b06af46e2c6e6ca71ff5b2
Kevin Wong <kev5ntech@gmail.com>
2024-10-22 11:53:14 -0700

    Initial Commit

---
+++ DiceView/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DiceView
+//
+//  Created by Kevin Wong on 10/22/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DiceView/DiceViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceViewApp.swift
+//  DiceView
+//
+//  Created by Kevin Wong on 10/22/24.
+//
+
+import SwiftUI
+
+@main
+struct DiceViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
